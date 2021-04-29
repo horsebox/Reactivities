179cd9b3cc8cad807c6e725669cf49723a95621a
Adam Ennis <adam.ennis@vizorsoftware.com>
2021-04-29 20:28:36 +0100

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -42,7 +42,14 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorPolicy", policy =>
+                
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000")
 
+                );
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +65,8 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorPolicy");
 
             app.UseAuthorization();
 
